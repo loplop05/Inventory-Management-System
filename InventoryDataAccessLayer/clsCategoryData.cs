8d029535c98275e2832061220f5acd36a724aa74
loplop05 <ammaralharoon@hotmail.com>
2026-07-18 21:28:20 +0300

    Createing Crud in DAL for Categories

--- InventoryDataAccessLayer/clsCategoryData.cs
+++ InventoryDataAccessLayer/clsCategoryData.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +13,252 @@ namespace InventoryDataAccessLayer
 {
     internal class clsCategoryData
     {
+
+        public static bool GetCategoryByID(ref int CategoryID ,ref string CaregoryName)
+        {
+
+            bool isFound = false;
+
+
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+
+            string query = @"SELECT * FROM CATEGORIES WHERE CategoryID = @CategoryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@CategoryID", CategoryID);
+
+            try
+            {
+                connection.Open();
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                if (reader.Read())
+                {
+                    isFound = true;
+
+                    CategoryID = (int)reader["CategoryID"];
+
+                    CaregoryName = (string)reader["CategoryName"];
+
+                }
+                else
+                {
+                    isFound = false;
+
+                }
+                reader.Close();
+            }
+
+
+            catch (Exception ex)
+            {
+                isFound = false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+
+        }
+       
+
+
+        public static int AddNewCategory(int CategoryID,string CategoryName)
+        {
+
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+            string query = @"insert into Categories (CategoryID,CategoryName)
+                            values (@CategoryID,@CategoryName) 
+                             select SCOPE_IDENTITY()";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            command.Parameters.AddWithValue(@"CategoryID", CategoryID);
+            command.Parameters.AddWithValue(@"CategoryName", CategoryName);
+
+
+            try
+            {
+                connection.Open();
+
+
+                object result = command.ExecuteScalar();
+
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    CategoryID = insertedID;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+            return CategoryID;
+
+        }
+
+       public static bool UpdateCategory(int CategoryID,string CategoryName)
+        {
+
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+            string query = @"UPDATE categories
+                            set CategoryName = @CategoryName
+                            where CategoryID = CategoryID";
+
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            int RowsAffected = 0;
+
+            command.Parameters.AddWithValue(@"CategoryID", CategoryID);
+            command.Parameters.AddWithValue(@"CategoryName", CategoryName);
+
+
+
+            try
+            {
+                connection.Open();
+                RowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return RowsAffected > 0;
+        }
+
+
+       public static bool DeleteCategory(int CategoryID)
+        {
+
+            int RowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+
+            string query = @"Delete Categories where CategoryID = @CategoryID";
+
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            command.Parameters.AddWithValue(@"CategoryID", CategoryID);
+
+            try
+            {
+                connection.Open();
+
+                RowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (RowsAffected > 0);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static DataTable GetAllContacts()
+        {
+            DataTable dt = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+            string query = @"select * from Categories";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            try
+            {
+                connection.Open();
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
