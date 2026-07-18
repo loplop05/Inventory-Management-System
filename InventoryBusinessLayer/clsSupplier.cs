001f85ee3dcef1c027aec53315aaaa7192b60c1d
loplop05 <ammaralharoon@hotmail.com>
2026-07-18 22:03:36 +0300

    Adding Supplier DAL + BL

--- InventoryBusinessLayer/clsSupplier.cs
+++ InventoryBusinessLayer/clsSupplier.cs
@@ -1,12 +1,264 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using InventoryDataAccessLayer;
+
 
 namespace InventoryBusinessLayer
 {
-    internal class clsSupplier
+
+    public class clsSupplier
     {
+
+
+
+        public enum enMode
+        {
+            AddNew = 0,
+            Update = 1
+        }
+
+
+
+
+
+        public enMode Mode = enMode.AddNew;
+
+        public int SupplierID;
+
+        public string SupplierName;
+
+        public string Phone;
+
+        public string Email;
+
+
+
+
+
+
+
+
+        public clsSupplier()
+        {
+
+            SupplierID = -1;
+
+            SupplierName = "";
+
+            Phone = "";
+
+            Email = "";
+
+        }
+
+
+
+
+
+
+
+
+        public clsSupplier(
+            int SupplierID,
+            string SupplierName,
+            string Phone,
+            string Email)
+        {
+
+
+            this.SupplierID = SupplierID;
+
+            this.SupplierName = SupplierName;
+
+            this.Phone = Phone;
+
+            this.Email = Email;
+
+
+
+            Mode = enMode.Update;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+        private bool _AddNewSupplier()
+        {
+
+
+            SupplierID =
+                clsSupplierData.AddNewSupplier(
+                    SupplierName,
+                    Phone,
+                    Email);
+
+
+
+            return SupplierID != -1;
+
+
+        }
+
+
+
+
+
+
+
+
+
+        private bool _UpdateSupplier()
+        {
+
+
+            return clsSupplierData.UpdateSupplier(
+                SupplierID,
+                SupplierName,
+                Phone,
+                Email);
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public static bool DeleteSupplier(int SupplierID)
+        {
+
+
+            return clsSupplierData.DeleteSupplier(SupplierID);
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public static clsSupplier FindSupplier(int SupplierID)
+        {
+
+
+            string SupplierName = "";
+
+            string Phone = "";
+
+            string Email = "";
+
+
+
+
+
+            if (clsSupplierData.GetSupplierByID(
+                SupplierID,
+                ref SupplierName,
+                ref Phone,
+                ref Email))
+            {
+
+
+                return new clsSupplier(
+                    SupplierID,
+                    SupplierName,
+                    Phone,
+                    Email);
+
+
+            }
+
+
+
+            return null;
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public static DataTable GetAllSuppliers()
+        {
+
+            return clsSupplierData.GetAllSuppliers();
+
+        }
+
+
+        public bool Save()
+        {
+
+
+            switch (Mode)
+            {
+
+
+                case enMode.AddNew:
+
+
+                    if (_AddNewSupplier())
+                    {
+
+
+                        Mode = enMode.Update;
+
+
+                        return true;
+
+
+                    }
+
+
+                    return false;
+
+
+
+
+
+
+
+                case enMode.Update:
+
+
+                    return _UpdateSupplier();
+
+
+            }
+
+
+
+            return false;
+
+
+        }
+
+
+
+
+
+
+
     }
+
 }
--- InventoryDataAccessLayer/clsSupplierData.cs
+++ InventoryDataAccessLayer/clsSupplierData.cs
@@ -1,12 +1,367 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace InventoryDataAccessLayer
 {
-    internal class clsSupplierData
+    public class clsSupplierData
     {
+
+
+        public static int AddNewSupplier(
+            string SupplierName,
+            string Phone,
+            string Email)
+        {
+
+            int SupplierID = -1;
+
+
+
+            using (SqlConnection connection =
+                new SqlConnection(clsDataAccessSettings.connectionString))
+            {
+
+
+                string query = @"
+                INSERT INTO Suppliers
+                (
+                    SupplierName,
+                    Phone,
+                    Email
+                )
+
+                VALUES
+                (
+                    @SupplierName,
+                    @Phone,
+                    @Email
+                );
+
+                SELECT SCOPE_IDENTITY();";
+
+
+
+                SqlCommand command =
+                    new SqlCommand(query, connection);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@SupplierName",
+                    SupplierName);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@Phone",
+                    Phone);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@Email",
+                    Email);
+
+
+
+
+                connection.Open();
+
+
+
+                object result =
+                    command.ExecuteScalar();
+
+
+
+                if (result != null)
+                {
+                    SupplierID =
+                        Convert.ToInt32(result);
+                }
+
+
+            }
+
+
+
+            return SupplierID;
+
+
+        }
+
+
+
+
+
+
+
+        public static bool UpdateSupplier(
+            int SupplierID,
+            string SupplierName,
+            string Phone,
+            string Email)
+        {
+
+
+            using (SqlConnection connection =
+                new SqlConnection(clsDataAccessSettings.connectionString))
+            {
+
+
+                string query = @"
+                UPDATE Suppliers
+
+                SET
+                    SupplierName=@SupplierName,
+                    Phone=@Phone,
+                    Email=@Email
+
+                WHERE SupplierID=@SupplierID";
+
+
+
+
+                SqlCommand command =
+                    new SqlCommand(query, connection);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@SupplierID",
+                    SupplierID);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@SupplierName",
+                    SupplierName);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@Phone",
+                    Phone);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@Email",
+                    Email);
+
+
+
+
+                connection.Open();
+
+
+
+                return command.ExecuteNonQuery() > 0;
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+        public static bool DeleteSupplier(int SupplierID)
+        {
+
+
+            using (SqlConnection connection =
+                new SqlConnection(clsDataAccessSettings.connectionString))
+            {
+
+
+                string query =
+                "DELETE FROM Suppliers WHERE SupplierID=@SupplierID";
+
+
+
+                SqlCommand command =
+                    new SqlCommand(query, connection);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@SupplierID",
+                    SupplierID);
+
+
+
+                connection.Open();
+
+
+
+                return command.ExecuteNonQuery() > 0;
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+        public static bool GetSupplierByID(
+            int SupplierID,
+            ref string SupplierName,
+            ref string Phone,
+            ref string Email)
+        {
+
+
+            bool IsFound = false;
+
+
+
+            using (SqlConnection connection =
+                new SqlConnection(clsDataAccessSettings.connectionString))
+            {
+
+
+
+                string query = @"
+                SELECT 
+                    SupplierName,
+                    Phone,
+                    Email
+
+                FROM Suppliers
+
+                WHERE SupplierID=@SupplierID";
+
+
+
+
+                SqlCommand command =
+                    new SqlCommand(query, connection);
+
+
+
+                command.Parameters.AddWithValue(
+                    "@SupplierID",
+                    SupplierID);
+
+
+
+                connection.Open();
+
+
+
+                SqlDataReader reader =
+                    command.ExecuteReader();
+
+
+
+                if (reader.Read())
+                {
+
+
+                    IsFound = true;
+
+
+
+                    SupplierName =
+                        reader["SupplierName"].ToString();
+
+
+
+                    Phone =
+                        reader["Phone"].ToString();
+
+
+
+                    Email =
+                        reader["Email"].ToString();
+
+
+                }
+
+
+
+                reader.Close();
+
+
+            }
+
+
+
+            return IsFound;
+
+
+        }
+
+
+
+
+
+
+
+
+        public static DataTable GetAllSuppliers()
+        {
+
+
+            DataTable dt = new DataTable();
+
+
+
+            using (SqlConnection connection =
+                new SqlConnection(clsDataAccessSettings.connectionString))
+            {
+
+
+                string query =
+                "SELECT * FROM Suppliers";
+
+
+
+                SqlCommand command =
+                    new SqlCommand(query, connection);
+
+
+
+                connection.Open();
+
+
+
+                SqlDataReader reader =
+                    command.ExecuteReader();
+
+
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+
+
+
+            }
+
+
+
+
+            return dt;
+
+
+        }
+
+
+
+
     }
 }
