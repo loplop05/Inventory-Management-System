9fb85d9bbc7abedc0b763fe3579f2d41cd969de4
loplop05 <ammaralharoon@hotmail.com>
2026-07-18 22:08:45 +0300

    Adding BL + DAL for products

--- InventoryBusinessLayer/clsProduct.cs
+++ InventoryBusinessLayer/clsProduct.cs
@@ -1,12 +1,141 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using InventoryDataAccessLayer;
 
 namespace InventoryBusinessLayer
 {
-    internal class clsProduct
+    public class clsProduct
     {
+        public enum enMode
+        {
+            AddNew = 0,
+            Update = 1
+        }
+
+
+        public enMode Mode = enMode.AddNew;
+
+
+        public int ProductID;
+        public string ProductName;
+        public int CategoryID;
+        public int SupplierID;
+        public decimal Price;
+        public int Quantity;
+        public string Barcode;
+        public string ImagePath;
+        public DateTime CreatedDate;
+
+
+
+        public clsProduct()
+        {
+            ProductID = -1;
+            ProductName = "";
+            CategoryID = -1;
+            SupplierID = -1;
+            Price = 0;
+            Quantity = 0;
+            Barcode = "";
+            ImagePath = "";
+            CreatedDate = DateTime.Now;
+        }
+
+
+
+        public clsProduct(
+            int ProductID,
+            string ProductName,
+            int CategoryID,
+            int SupplierID,
+            decimal Price,
+            int Quantity,
+            string Barcode,
+            string ImagePath,
+            DateTime CreatedDate)
+        {
+            this.ProductID = ProductID;
+            this.ProductName = ProductName;
+            this.CategoryID = CategoryID;
+            this.SupplierID = SupplierID;
+            this.Price = Price;
+            this.Quantity = Quantity;
+            this.Barcode = Barcode;
+            this.ImagePath = ImagePath;
+            this.CreatedDate = CreatedDate;
+
+            Mode = enMode.Update;
+        }
+
+
+
+        private bool _AddNewProduct()
+        {
+            ProductID = clsProductData.AddNewProduct(
+                ProductName,
+                CategoryID,
+                SupplierID,
+                Price,
+                Quantity,
+                Barcode,
+                ImagePath,
+                CreatedDate);
+
+            return ProductID != -1;
+        }
+
+
+
+        private bool _UpdateProduct()
+        {
+            return clsProductData.UpdateProduct(
+                ProductID,
+                ProductName,
+                CategoryID,
+                SupplierID,
+                Price,
+                Quantity,
+                Barcode,
+                ImagePath);
+        }
+
+
+
+        public static bool DeleteProduct(int ProductID)
+        {
+            return clsProductData.DeleteProduct(ProductID);
+        }
+
+
+
+        public static DataTable GetAllProducts()
+        {
+            return clsProductData.GetAllProducts();
+        }
+
+
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+
+                    if (_AddNewProduct())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+
+                    return false;
+
+
+                case enMode.Update:
+
+                    return _UpdateProduct();
+            }
+
+            return false;
+        }
     }
 }
--- InventoryDataAccessLayer/clsProductData.cs
+++ InventoryDataAccessLayer/clsProductData.cs
@@ -1,12 +1,172 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace InventoryDataAccessLayer
 {
-    internal class clsProductData
+    public class clsProductData
     {
+        public static int AddNewProduct(
+            string ProductName,
+            int CategoryID,
+            int SupplierID,
+            decimal Price,
+            int Quantity,
+            string Barcode,
+            string ImagePath,
+            DateTime CreatedDate)
+        {
+            int ProductID = -1;
+
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = @"INSERT INTO Products
+                                (ProductName, CategoryID, SupplierID, Price, Quantity, Barcode, ImagePath, CreatedDate)
+
+                                VALUES
+
+                                (@ProductName,@CategoryID,@SupplierID,@Price,@Quantity,@Barcode,@ImagePath,@CreatedDate);
+
+                                SELECT SCOPE_IDENTITY();";
+
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                command.Parameters.AddWithValue("@ProductName", ProductName);
+                command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                command.Parameters.AddWithValue("@SupplierID", SupplierID);
+                command.Parameters.AddWithValue("@Price", Price);
+                command.Parameters.AddWithValue("@Quantity", Quantity);
+                command.Parameters.AddWithValue("@Barcode", Barcode);
+                command.Parameters.AddWithValue("@ImagePath", (object)ImagePath ?? DBNull.Value);
+                command.Parameters.AddWithValue("@CreatedDate", CreatedDate);
+
+
+                connection.Open();
+
+
+                object result = command.ExecuteScalar();
+
+
+                if (result != null)
+                    ProductID = Convert.ToInt32(result);
+            }
+
+
+            return ProductID;
+        }
+
+
+
+        public static bool UpdateProduct(
+            int ProductID,
+            string ProductName,
+            int CategoryID,
+            int SupplierID,
+            decimal Price,
+            int Quantity,
+            string Barcode,
+            string ImagePath)
+        {
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = @"UPDATE Products
+
+                                 SET ProductName=@ProductName,
+                                     CategoryID=@CategoryID,
+                                     SupplierID=@SupplierID,
+                                     Price=@Price,
+                                     Quantity=@Quantity,
+                                     Barcode=@Barcode,
+                                     ImagePath=@ImagePath
+
+                                 WHERE ProductID=@ProductID";
+
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                command.Parameters.AddWithValue("@ProductID", ProductID);
+                command.Parameters.AddWithValue("@ProductName", ProductName);
+                command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                command.Parameters.AddWithValue("@SupplierID", SupplierID);
+                command.Parameters.AddWithValue("@Price", Price);
+                command.Parameters.AddWithValue("@Quantity", Quantity);
+                command.Parameters.AddWithValue("@Barcode", Barcode);
+                command.Parameters.AddWithValue("@ImagePath", (object)ImagePath ?? DBNull.Value);
+
+
+                connection.Open();
+
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+
+
+
+        public static bool DeleteProduct(int ProductID)
+        {
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = "DELETE FROM Products WHERE ProductID=@ProductID";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@ProductID", ProductID);
+
+                connection.Open();
+
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+
+
+
+        public static DataTable GetAllProducts()
+        {
+            DataTable dt = new DataTable();
+
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = @"
+                SELECT 
+                    Products.ProductID,
+                    Products.ProductName,
+                    Categories.CategoryName,
+                    Suppliers.SupplierName,
+                    Products.Price,
+                    Products.Quantity,
+                    Products.Barcode,
+                    Products.ImagePath,
+                    Products.CreatedDate
+
+                FROM Products
+
+                INNER JOIN Categories
+                ON Products.CategoryID = Categories.CategoryID
+
+                INNER JOIN Suppliers
+                ON Products.SupplierID = Suppliers.SupplierID";
+
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                connection.Open();
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+            }
+
+
+            return dt;
+        }
     }
 }
