a749936fcb797697229dde1c338c053f68a45481
loplop05 <ammaralharoon@hotmail.com>
2026-08-10 04:00:13 +0300

    Split Method for payment

--- frmPaymentSelection.Designer.cs
+++ frmPaymentSelection.Designer.cs
@@ -16,11 +16,7 @@ namespace InventoryManagementSystem
         private void InitializeComponent()
         {
             this._lblTotalAmount = new System.Windows.Forms.Label();
-            this._rbCash = new System.Windows.Forms.RadioButton();
-            this._rbCard = new System.Windows.Forms.RadioButton();
-            this._rbSplit = new System.Windows.Forms.RadioButton();
-            this._lblCardLastFour = new System.Windows.Forms.Label();
-            this._txtCardLastFour = new System.Windows.Forms.TextBox();
+            this._lblPaymentMethods = new System.Windows.Forms.Label();
             this._btnCash = new System.Windows.Forms.Button();
             this._btnCard = new System.Windows.Forms.Button();
             this._btnSplit = new System.Windows.Forms.Button();
@@ -30,100 +26,65 @@ namespace InventoryManagementSystem
             // _lblTotalAmount
             // 
             this._lblTotalAmount.AutoSize = true;
-            this._lblTotalAmount.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
-            this._lblTotalAmount.Location = new System.Drawing.Point(20, 20);
+            this._lblTotalAmount.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this._lblTotalAmount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this._lblTotalAmount.Location = new System.Drawing.Point(30, 30);
             this._lblTotalAmount.Name = "_lblTotalAmount";
-            this._lblTotalAmount.Size = new System.Drawing.Size(140, 24);
+            this._lblTotalAmount.Size = new System.Drawing.Size(180, 32);
             this._lblTotalAmount.TabIndex = 0;
-            this._lblTotalAmount.Text = "Total Amount: $0.00";
+            this._lblTotalAmount.Text = "Total: $0.00";
             // 
-            // _rbCash
+            // _lblPaymentMethods
             // 
-            this._rbCash.AutoSize = true;
-            this._rbCash.Checked = true;
-            this._rbCash.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this._rbCash.Location = new System.Drawing.Point(20, 60);
-            this._rbCash.Name = "_rbCash";
-            this._rbCash.Size = new System.Drawing.Size(55, 19);
-            this._rbCash.TabIndex = 1;
-            this._rbCash.TabStop = true;
-            this._rbCash.Text = "Cash";
-            this._rbCash.UseVisualStyleBackColor = true;
-            // 
-            // _rbCard
-            // 
-            this._rbCard.AutoSize = true;
-            this._rbCard.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this._rbCard.Location = new System.Drawing.Point(20, 90);
-            this._rbCard.Name = "_rbCard";
-            this._rbCard.Size = new System.Drawing.Size(48, 19);
-            this._rbCard.TabIndex = 2;
-            this._rbCard.Text = "Card";
-            this._rbCard.UseVisualStyleBackColor = true;
-            // 
-            // _rbSplit
-            // 
-            this._rbSplit.AutoSize = true;
-            this._rbSplit.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this._rbSplit.Location = new System.Drawing.Point(20, 120);
-            this._rbSplit.Name = "_rbSplit";
-            this._rbSplit.Size = new System.Drawing.Size(51, 19);
-            this._rbSplit.TabIndex = 3;
-            this._rbSplit.Text = "Split";
-            this._rbSplit.UseVisualStyleBackColor = true;
-            // 
-            // _lblCardLastFour
-            // 
-            this._lblCardLastFour.AutoSize = true;
-            this._lblCardLastFour.Location = new System.Drawing.Point(20, 150);
-            this._lblCardLastFour.Name = "_lblCardLastFour";
-            this._lblCardLastFour.Size = new System.Drawing.Size(82, 13);
-            this._lblCardLastFour.TabIndex = 4;
-            this._lblCardLastFour.Text = "Last 4 Digits:";
-            // 
-            // _txtCardLastFour
-            // 
-            this._txtCardLastFour.Location = new System.Drawing.Point(110, 147);
-            this._txtCardLastFour.Name = "_txtCardLastFour";
-            this._txtCardLastFour.Size = new System.Drawing.Size(80, 20);
-            this._txtCardLastFour.TabIndex = 5;
+            this._lblPaymentMethods.AutoSize = true;
+            this._lblPaymentMethods.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular);
+            this._lblPaymentMethods.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(125)))), ((int)(((byte)(139)))));
+            this._lblPaymentMethods.Location = new System.Drawing.Point(30, 80);
+            this._lblPaymentMethods.Name = "_lblPaymentMethods";
+            this._lblPaymentMethods.Size = new System.Drawing.Size(130, 21);
+            this._lblPaymentMethods.TabIndex = 1;
+            this._lblPaymentMethods.Text = "Select Payment:";
             // 
             // _btnCash
             // 
-            this._btnCash.Location = new System.Drawing.Point(20, 190);
+            this._btnCash.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this._btnCash.Location = new System.Drawing.Point(30, 120);
             this._btnCash.Name = "_btnCash";
-            this._btnCash.Size = new System.Drawing.Size(100, 35);
-            this._btnCash.TabIndex = 6;
-            this._btnCash.Text = "Cash";
+            this._btnCash.Size = new System.Drawing.Size(140, 50);
+            this._btnCash.TabIndex = 2;
+            this._btnCash.Text = "💵 Cash";
             this._btnCash.UseVisualStyleBackColor = true;
             this._btnCash.Click += new System.EventHandler(this._btnCash_Click);
             // 
             // _btnCard
             // 
-            this._btnCard.Location = new System.Drawing.Point(130, 190);
+            this._btnCard.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this._btnCard.Location = new System.Drawing.Point(190, 120);
             this._btnCard.Name = "_btnCard";
-            this._btnCard.Size = new System.Drawing.Size(100, 35);
-            this._btnCard.TabIndex = 7;
-            this._btnCard.Text = "Card";
+            this._btnCard.Size = new System.Drawing.Size(140, 50);
+            this._btnCard.TabIndex = 3;
+            this._btnCard.Text = "💳 Card";
             this._btnCard.UseVisualStyleBackColor = true;
             this._btnCard.Click += new System.EventHandler(this._btnCard_Click);
             // 
             // _btnSplit
             // 
-            this._btnSplit.Location = new System.Drawing.Point(240, 190);
+            this._btnSplit.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this._btnSplit.Location = new System.Drawing.Point(350, 120);
             this._btnSplit.Name = "_btnSplit";
-            this._btnSplit.Size = new System.Drawing.Size(100, 35);
-            this._btnSplit.TabIndex = 8;
-            this._btnSplit.Text = "Split";
+            this._btnSplit.Size = new System.Drawing.Size(140, 50);
+            this._btnSplit.TabIndex = 4;
+            this._btnSplit.Text = "💰 Split";
             this._btnSplit.UseVisualStyleBackColor = true;
             this._btnSplit.Click += new System.EventHandler(this._btnSplit_Click);
             // 
             // _btnCancel
             // 
-            this._btnCancel.Location = new System.Drawing.Point(350, 190);
+            this._btnCancel.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this._btnCancel.Location = new System.Drawing.Point(390, 200);
             this._btnCancel.Name = "_btnCancel";
             this._btnCancel.Size = new System.Drawing.Size(100, 35);
-            this._btnCancel.TabIndex = 9;
+            this._btnCancel.TabIndex = 5;
             this._btnCancel.Text = "Cancel";
             this._btnCancel.UseVisualStyleBackColor = true;
             this._btnCancel.Click += new System.EventHandler(this._btnCancel_Click);
@@ -132,16 +93,12 @@ namespace InventoryManagementSystem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 240);
+            this.ClientSize = new System.Drawing.Size(520, 260);
             this.Controls.Add(this._btnCancel);
             this.Controls.Add(this._btnSplit);
             this.Controls.Add(this._btnCard);
             this.Controls.Add(this._btnCash);
-            this.Controls.Add(this._txtCardLastFour);
-            this.Controls.Add(this._lblCardLastFour);
-            this.Controls.Add(this._rbSplit);
-            this.Controls.Add(this._rbCard);
-            this.Controls.Add(this._rbCash);
+            this.Controls.Add(this._lblPaymentMethods);
             this.Controls.Add(this._lblTotalAmount);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -155,14 +112,11 @@ namespace InventoryManagementSystem
         }
 
         private System.Windows.Forms.Label _lblTotalAmount;
-        private System.Windows.Forms.RadioButton _rbCash;
-        private System.Windows.Forms.RadioButton _rbCard;
-        private System.Windows.Forms.RadioButton _rbSplit;
-        private System.Windows.Forms.Label _lblCardLastFour;
-        private System.Windows.Forms.TextBox _txtCardLastFour;
+        private System.Windows.Forms.Label _lblPaymentMethods;
         private System.Windows.Forms.Button _btnCash;
         private System.Windows.Forms.Button _btnCard;
         private System.Windows.Forms.Button _btnSplit;
         private System.Windows.Forms.Button _btnCancel;
+
     }
 }
--- frmPaymentSelection.cs
+++ frmPaymentSelection.cs
@@ -31,20 +31,7 @@ namespace InventoryManagementSystem
 
             // Set initial values
             _lblTotalAmount.Text = "Total Amount: " + TotalAmount.ToString("C2");
-            _rbCash.Checked = true;
             _txtCardLastFour.MaxLength = 4;
-            _txtCardLastFour.Enabled = false;
-
-            // Wire events
-            _rbCash.CheckedChanged += PaymentMethodChanged;
-            _rbCard.CheckedChanged += PaymentMethodChanged;
-            _rbSplit.CheckedChanged += PaymentMethodChanged;
-        }
-
-        private void PaymentMethodChanged(object sender, EventArgs e)
-        {
-            _txtCardLastFour.Enabled = _rbCard.Checked || _rbSplit.Checked;
-            _btnSplit.Enabled = _rbSplit.Checked;
         }
 
         private void _btnCash_Click(object sender, EventArgs e)
@@ -57,53 +44,34 @@ namespace InventoryManagementSystem
 
         private void _btnCard_Click(object sender, EventArgs e)
         {
-            if (_rbCard.Checked || _rbSplit.Checked)
+            // Show card input dialog
+            using (var cardInput = new frmInputBox("Enter last 4 digits of card:", "Card Payment"))
             {
-                if (string.IsNullOrWhiteSpace(_txtCardLastFour.Text))
+                if (cardInput.ShowDialog(this) != DialogResult.OK)
+                    return;
+
+                if (string.IsNullOrWhiteSpace(cardInput.InputValue))
                 {
                     clsFormTheme.ShowError(this, "Please enter the last 4 digits of the card.", "Missing Card Info");
                     return;
                 }
 
-                if (_txtCardLastFour.Text.Length != 4)
+                if (cardInput.InputValue.Length != 4)
                 {
                     clsFormTheme.ShowError(this, "Card last 4 digits must be exactly 4 characters.", "Invalid Card Info");
                     return;
                 }
-            }
 
-            if (_rbCard.Checked)
-            {
                 SelectedPaymentMethod = "Visa";
-                PaymentDetails = "****" + _txtCardLastFour.Text;
+                PaymentDetails = "****" + cardInput.InputValue;
+                DialogResult = DialogResult.OK;
+                Close();
             }
-            else if (_rbSplit.Checked)
-            {
-                // Show split payment dialog
-                using (var splitDialog = new frmSplitPayment(TotalAmount))
-                {
-                    if (splitDialog.ShowDialog(this) == DialogResult.OK)
-                    {
-                        SelectedPaymentMethod = "Split";
-                        PaymentDetails = $"Cash:{splitDialog.CashAmount:0.00}|Card:{splitDialog.CardAmount:0.00}|{splitDialog.CardType}:****{splitDialog.CardLastFour}";
-                        DialogResult = DialogResult.OK;
-                        Close();
-                    }
-                    return;
-                }
-            }
-            else
-            {
-                SelectedPaymentMethod = "Cash";
-                PaymentDetails = null;
-            }
-
-            DialogResult = DialogResult.OK;
-            Close();
         }
 
         private void _btnSplit_Click(object sender, EventArgs e)
         {
+            // Show split payment dialog
             using (var splitDialog = new frmSplitPayment(TotalAmount))
             {
                 if (splitDialog.ShowDialog(this) == DialogResult.OK)
